63415b1f50f7d1535dab8f1b9bc3ddd21be392fc
CelineCambier <cambier.celine2@gmail.com>
2024-09-24 20:23:53 -0700

    Added Sidebar css

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,19 @@
-
 export default function SideBar() {
   return (
-      <div>
-          <h2>The Brutal martian landscape</h2>
-          <div>
-              <p>Description</p>
-              <p>hmfg weeg wgsdgnj rghethg wshthbdjn gehgsz thryhw jrdgsfq whsrhgw</p>
-          </div>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal martian landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            hmfg weeg wgsdgnj rghethg wshthbdjn gehgsz thryhw jrdgsfq whsrhgw
+          </p>
+        </div>
+        <button>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
