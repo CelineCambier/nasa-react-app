62525263773c8fedfa8b090de90cd864c37b12a6
CelineCambier <cambier.celine2@gmail.com>
2024-09-24 20:08:18 -0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="bgGradient"></div>
+        <div>
+          <h2>The Brutal Martian Landscape</h2>
+          <h1>WALL-E PROJECT</h1>
+        </div>
+        <button>
+          <i className="fa-solid fa-circle-info"></i>
+        </button>
+      </footer>
+    );
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+
+export default function SideBar() {
+  return (
+      <div>
+          <h2>The Brutal martian landscape</h2>
+          <div>
+              <p>Description</p>
+              <p>hmfg weeg wgsdgnj rghethg wshthbdjn gehgsz thryhw jrdgsfq whsrhgw</p>
+          </div>
+    </div>
+  )
+}
